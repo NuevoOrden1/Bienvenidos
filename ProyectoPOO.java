d5b71ece5fd65a25e8f02d88e6199be2831a4c0c
Alan <131622558+NuevoOrden1@users.noreply.github.com>
2023-11-12 19:58:55 -0500

    Update ProyectoPOO.java

--- ProyectoPOO.java
+++ ProyectoPOO.java
@@ -1,8 +1,41 @@
-public class{
+public class ProyectoPOO{
   public static void main (String [] args){
     int a;
     float b;
     double c;
   }
 
+}
+
+public class ticket{
+import java.util.LinkedList;
+
+public class LinkedListRemoveExample {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        // Agregar elementos a la LinkedList
+        linkedList.add("A");
+        linkedList.add("B");
+        linkedList.add("C");
+        linkedList.add("D");
+        linkedList.add("E");
+
+        // Imprimir la LinkedList original
+        System.out.println("Original LinkedList: " + linkedList);
+
+        // Eliminar el elemento en el medio (índice 2)
+        linkedList.remove(2);
+
+        // Imprimir la LinkedList después de la eliminación
+        System.out.println("LinkedList después de eliminar en el medio: " + linkedList);
+    }
+}
+
+  
+}
+
+public class ticket{
+
+  
 }
