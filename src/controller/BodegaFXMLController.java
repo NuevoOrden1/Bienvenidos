b2cc04cb8116e8652cca48cfa49e24790a43fba9
Alan <131622558+NuevoOrden1@users.noreply.github.com>
2023-11-29 02:25:45 -0500

    actualize BodegaFXMLController.java
    solo modificar la ruta de la base de datos
    
    String url = "jdbc:sqlite:/home/bodegaApp/productosPrueba.db";
    
    se modifica a partir de  "/home/...." ;

--- src/controller/BodegaFXMLController.java
+++ src/controller/BodegaFXMLController.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
@@ -17,11 +18,19 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Calendario;
-
+//clases y metodos relacionados con la gestion de base de datos JDBC
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  * FXML Controller class
  *
- * @author Jesus Diego
+ * @author LosMosqueteros
  */
 public class BodegaFXMLController implements Initializable {
 
@@ -30,11 +39,11 @@ public class BodegaFXMLController implements Initializable {
      */
     
     @FXML private Button btnAñadirCarrito;
-    @FXML private Button btnConsultarProducto;
+    @FXML private Button btnBuscarProducto;
     @FXML private Button btnEmitirBoleta;
     @FXML private TextField tfCodigo;
     @FXML private TextField tfNombreProducto;
-    @FXML private TextField tfCantidad;
+    @FXML private TextField tfCantidad; //esto no es un dato de base.
     @FXML private TextField tfTipo;
     @FXML private TextField tfPrecioUnitarioCosto;
     @FXML private TextField tfPrecioUnitarioVenta;
@@ -50,7 +59,51 @@ public class BodegaFXMLController implements Initializable {
         
     }
     
-    @FXML private void accionConsultarProducto(){
+    @FXML private void accionBuscarProducto(){
+          String codigoIngresado = tfCodigo.getText();//tfCodigoTextField.getText();
+
+    // Validar si el campo está vacío o no es un número
+    if (codigoIngresado.isEmpty() || !esNumeroValido(codigoIngresado)) {
+        mostrarAlerta("Entrada inválida", "Ingrese un número válido antes de buscar.");
+        return; // Salir del método si la entrada no es válida
+    }
+
+    // Configuración de la conexión a SQLite
+    String url = "jdbc:sqlite:/home/alan/NetBeansProjects/bodegaApp/productosPrueba.db";
+
+    try (Connection connection = DriverManager.getConnection(url)) {
+        // Consulta SQL para obtener todos los campos del producto según el ID ingresado
+        String sql = "SELECT * FROM productos WHERE id_productos = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            // Convertir la entrada a un entero y establecer el valor del parámetro
+            int id = Integer.parseInt(codigoIngresado);
+            preparedStatement.setInt(1, id);
+
+            // Ejecutar la consulta
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    // Procesar el resultado y autocompletar los campos
+                    //codigoBarrasTextField.setText(resultSet.getString("codigo_barras"));
+                    tfTipo.setText(resultSet.getString("tipo"));
+                    tfStock.setText(Integer.toString(resultSet.getInt("stock_unidades")));
+                    tfNombreProducto.setText(resultSet.getString("nombre"));
+                    //descripcionTextField.setText(resultSet.getString("descripcion"));
+                    tfPrecioUnitarioCosto.setText(Double.toString(resultSet.getDouble("precio_unitario_costo")));
+                    tfLote.setText(resultSet.getString("fecha_de_caducidad"));
+                    tfPrecioUnitarioVenta.setText(Double.toString(resultSet.getDouble("precio_unitario_venta")));
+                } else {
+                    // Limpiar todos los campos si no se encuentra el producto
+                    limpiarCampos();
+
+                    // Mostrar un mensaje de alerta
+                    mostrarAlerta("Producto no encontrado", "El producto con ID " + id + " no fue encontrado.");
+                }
+            }
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+      }
         
     }
     
@@ -67,4 +120,37 @@ public class BodegaFXMLController implements Initializable {
             e.printStackTrace();
         }
     }
+    
+    
+    private void limpiarCampos() {
+    
+    tfCodigo.clear(); //errores: tfCodigoTextField.clear();
+    tfStock.clear();
+    tfTipo.clear();
+    tfNombreProducto.clear();
+    
+    tfPrecioUnitarioCosto.clear();
+    tfLote.clear();
+    tfPrecioUnitarioVenta.clear();
+    }
+    
+    private void mostrarAlerta(String titulo, String contenido) {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setTitle(titulo);
+    alert.setHeaderText(null);
+    alert.setContentText(contenido);
+    alert.showAndWait();
+    }
+
+    private boolean esNumeroValido(String texto) {
+    try {
+        Integer.parseInt(texto);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
+      }
+    }
+    
+    
+    
 }
